1623506001eb4b12f90127cee56a0df354a060b5
Maicon <maicon-gil@PRO-PAE-0483.PROCERGS.REDERS>
2017-02-03 17:13:13 -0200

    Fix ipaddress format

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ var express = require("express"),
 app.use(morgan('combined'));
 
 app.get("*", function(req, res){
+
+    var ipaddress = req.ip;
+    if (ipaddress.substr(0, 7) == "::ffff:") {
+        ipaddress = ipaddress.substr(7)
+    }
+
+    var language = req.headers['accept-language'].substring(0,req.headers['accept-language'].indexOf(","));
+    var software = req.headers['user-agent'].match(/\(([^)]+)\)/)[1];
+
     var output = {
-        ipaddress : req.ip,
-        language : req.headers['accept-language'].substring(0,req.headers['accept-language'].indexOf(",")),
-        software : req.headers['user-agent'].match(/\(([^)]+)\)/)[1]
+        ipaddress : ipaddress,
+        language : language,
+        software : software
     };
+
     res.send(output);
 });
 
